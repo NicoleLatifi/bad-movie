943961d56446dfabd5f4e0f26c57b9ae8680ec46
Tyler Haglund <relyt4me@gmail.com>
2020-08-26 19:42:44 -0600

    Remove unused method

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
-import badlogo from '../badlogo.png';
-import { Link } from 'react-router-dom'
+import badLogo from '../badlogo.png';
+import { Link } from 'react-router-dom';
 
-const Header = ({ currentUser, toggleLoginModal, logoutUser }) => {
+const Header = ({ currentUser, logoutUser }) => {
   return (
     <header className='header'>
-      <img src={badlogo} alt='Bad Movie Logo' height='50' width='50' />
+      <img src={badLogo} alt='Bad Movie Logo' height='50' width='50' />
       {currentUser && <h2>Hello {currentUser.name}</h2>}
       {currentUser && (
         <button className='logout-button' onClick={logoutUser}>
@@ -15,9 +15,7 @@ const Header = ({ currentUser, toggleLoginModal, logoutUser }) => {
       )}
       {!currentUser && (
         <Link to='/login'>
-          <button className='login-button'>
-            Login
-          </button>
+          <button className='login-button'>Login</button>
         </Link>
       )}
     </header>
