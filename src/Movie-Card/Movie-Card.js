ac5d6f646dcf3def7e01e2e7afcb528871b74222
Tyler Haglund <relyt4me@gmail.com>
2020-08-26 20:43:06 -0600

    Change the moviecard to functional component

--- src/Movie-Card/Movie-Card.js
+++ src/Movie-Card/Movie-Card.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie-Card.css';
 
-class MovieCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: '',
-    };
+const MovieCard = () => {
+  let usersRating;
+  const { currentUser, movie } = this.props;
+  if (currentUser) {
+    usersRating = currentUser.ratings.find((rating) => {
+      return movie.id === rating.movie_id;
+    });
   }
 
-  render() {
-    let usersRating;
-    const { currentUser, movie } = this.props;
-    if (currentUser) {
-      usersRating = currentUser.ratings.find((rating) => {
-        return movie.id === rating.movie_id;
-      });
-    }
-    return (
-      <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }} alt={`background image of ${movie.title} poster`}>
-        {this.state.error && <p>Sorry, no movie details to display.</p>}
-        <p className='movie-card-rating'>
-          Avg<br></br>
-          {Math.round(movie.average_rating * 10) / 10}/10
+  return (
+    <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }} alt={`background image of ${movie.title} poster`}>
+      <p className='movie-card-rating'>
+        Avg<br></br>
+        {Math.round(movie.average_rating * 10) / 10}/10
+      </p>
+      {usersRating && (
+        <p className='movie-card-user-rating'>
+          My Rating<br></br>
+          {usersRating.rating}/10
         </p>
-        {usersRating && (
-          <p className='movie-card-user-rating'>
-            My Rating<br></br>
-            {usersRating.rating}/10
-          </p>
-        )}
-      </article>
-    );
-  }
-}
+      )}
+    </article>
+  );
+};
 
 export default MovieCard;
