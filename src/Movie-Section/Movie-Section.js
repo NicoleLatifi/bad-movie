0ce83a22b3bd293379a16a385f3347c14ead0491
Tyler Haglund <relyt4me@gmail.com>
2020-08-26 19:53:27 -0600

    Change MovieSection into a functional component

--- src/Movie-Section/Movie-Section.js
+++ src/Movie-Section/Movie-Section.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from '../Movie-Card/Movie-Card';
 import './Movie-Section.css';
 import { Link } from 'react-router-dom';
 
-class MovieSection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const movieCards = this.props.movies.map((movie) => {
-      return (
-        <Link to={`/movies/${movie.id}`}>
-          <MovieCard movie={movie} currentUser={this.props.currentUser} />
-        </Link>
-      );
-    });
-
+const MovieSection = ({ movies, currentUser }) => {
+  const movieCards = movies.map((movie) => {
     return (
-      <section className='movies-section'>
-        <h2 className='movies-section-header'>Movies</h2>
-        <div className='movies-section-container'>{movieCards}</div>
-      </section>
+      <Link to={`/movies/${movie.id}`}>
+        <MovieCard movie={movie} currentUser={currentUser} />
+      </Link>
     );
-  }
-}
+  });
+
+  return (
+    <section className='movies-section'>
+      <h2 className='movies-section-header'>Movies</h2>
+      <div className='movies-section-container'>{movieCards}</div>
+    </section>
+  );
+};
 
 export default MovieSection;
