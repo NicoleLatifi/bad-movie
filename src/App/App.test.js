2085517f1adffc9c344b8701c4cfda05a9d9cc82
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-31 14:46:11 -0600

    Refactor variable format to make test pass

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -9,7 +9,7 @@ import '@testing-library/jest-dom';
 jest.mock('../Fetch.js');
 
 describe('App Tests', () => {
-  let movies,selectedMovie;
+  let movies;
   beforeEach(() => {
     movies = [
       {
@@ -50,26 +50,41 @@ describe('App Tests', () => {
     );
 
     const header = await waitFor(() => screen.getByRole('heading', {name: 'Movies'} ))
-    const movieCardLink = await waitFor(() => screen.getByAltText('Greenland poster')); // what else would go here?
+    const movieAvgRating = await waitFor(() => screen.getByText('Avg8/10'));
+    const movieCardLink = await waitFor(() => screen.getByTestId('1'));
 
+    expect(movieAvgRating).toBeInTheDocument();
     expect(header).toBeInTheDocument();
     expect(movieCardLink).toBeInTheDocument();
   })
 
   it('Should render a Movie Page when a Movie Card is clicked', async () => {
-    selectedMovie = {
+    const singleMovie = {
       data: {
-        id: 1,
-        title: 'Greenland',
-        poster_path: "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
-        backdrop_path: "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
-        overview: 'Greenland overview',
-        average_rating: 8
-      }
+        movie: {
+            id: 1,
+            title: "Greenland",
+            poster_path: "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
+            backdrop_path: "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
+            release_date: "2020-07-29",
+            overview: 'Greenland overview',
+            genres: [
+                "Action",
+                "Science Fiction",
+                "Thriller"
+            ],
+            budget: 0,
+            revenue: 0,
+            runtime: 119,
+            tagline: "",
+            average_rating: 8
+        }
+      },
+      error: false
     };
 
     getAllMovies.mockResolvedValueOnce(movies);
-    getSingleMovie.mockResolvedValueOnce(selectedMovie);
+    getSingleMovie.mockResolvedValueOnce(singleMovie);
 
     render(
       <MemoryRouter>
@@ -78,25 +93,16 @@ describe('App Tests', () => {
     );
 
     const header = await waitFor(() => screen.getByRole('heading', {name: 'Movies'} ))
-    const movieCardLink = await waitFor(() => screen.getByAltText('Greenland poster')); // what else would go here?
-
-    selectedMovie = {
-      id: 1,
-      title: 'Greenland',
-      poster_path: "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
-      backdrop_path: "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
-      overview: 'Greenland overview',
-      average_rating: 8
-    };
+    const movieCardLink = await waitFor(() => screen.getByTestId('1'));
 
     await fireEvent.click(movieCardLink);
 
-    const greenlandOverview = await waitFor(() => screen.getByRole('paragraph', {name: 'Greenland overview'}));
+    const greenlandHeading = await waitFor(() => screen.getByRole('heading', {name: 'Greenland'}));
+    const greenlandOverview = await waitFor(() => screen.getByText('Greenland overview'));
 
-    expect(header).toBeInTheDocument();
-    expect(movieCardLink).toBeInTheDocument();
+    expect(header).not.toBeInTheDocument();
+    expect(movieCardLink).not.toBeInTheDocument();
+    expect(greenlandHeading).toBeInTheDocument();
     expect(greenlandOverview).toBeInTheDocument();
   })
 })
-
-// The first test passes if you change what's rendered in Movie-Card (see commented out code there)
